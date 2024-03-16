7359603670d5b1a9381c4559246e0948606534ec
Nolan Hovis <nolan.hovisPersonal@gmail.com>
2024-03-16 16:01:00 -0500

    pusher updates

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,8 @@
 export const environment = {
-  production: false, 
-  apiUrl: "http://localhost:3000"
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  pusher: {
+    key: '04638eae8087dc5495ac',
+    cluster: 'us2',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
-  production: true, 
-  apiUrl: "https://betablogsgf.onrender.com"
+  production: true,
+  apiUrl: 'https://betablogsgf.onrender.com',
+  pusher: {
+    key: '04638eae8087dc5495ac',
+    cluster: 'us2',
+  },
 };
